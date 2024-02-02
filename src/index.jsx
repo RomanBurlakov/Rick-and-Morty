9d49e612716335e2b78194ec5d4b91828ae416bf
RomanBurlakov <roma160398@yandex.ru>
2024-02-02 03:48:58 +0300

    add router

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import './style/style.scss';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter>
+    <HashRouter basename='/Rick-and-Morty' hashType='slash'>
         <App />
     </HashRouter>
 );
