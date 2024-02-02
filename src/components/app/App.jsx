13f90c9d37a1ba503869c32ab18687eef1b5f47b
RomanBurlakov <roma160398@yandex.ru>
2024-02-02 03:38:54 +0300

    change path

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -14,6 +14,7 @@ function App() {
 			<main className='app__content'>
 				<Routes>
 					<Route path='/' element={<Redirect path='/characters' />} />
+					<Route path='/Rick-and-Morty' element={<Redirect path='/characters' />} />
 					<Route path='/characters' element={<MainPage />} />
 					<Route path='/characters/:id' element={<MainPage />} />
 					<Route path='/episodes' element={<Episodes />} />
